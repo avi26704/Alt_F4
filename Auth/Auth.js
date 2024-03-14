d68aee84295f19b4b8fdcf0266447e10dc2191de
yuvrajpradhan <Yuvraj.pradhan.2004@gmail.com>
2024-03-15 01:26:53 +0530

    backend update 2
    upto register and login route done
    hashing left and tokens also left

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,3 +1,4 @@
+
 const User = require("./user.js")
 exports.register = async (req, res, next) => {
     const { Registration_number, password, role, block,Phone_number,room_number,  email,} = req.body
@@ -22,6 +23,41 @@ exports.register = async (req, res, next) => {
       res.status(401).json({
         message: "User not successful created",
         error: error.mesage,
+      })
+    }
+  }
+
+  //login function
+  exports.login = async (req, res, next) => {
+    const { username, password } = req.body
+    // Check if username and password is provided
+    if (!username || !password) {
+      return res.status(400).json({
+        message: "Username or Password not present",
+      })
+    }
+    try {
+      const user = await User.findOne({ username })
+      if (!user) {
+        res.status(400).json({
+          message: "Login not successful",
+          error: "User not found",
+        })
+      } else {
+        // comparing given password with hashed password
+        bcrypt.compare(password, user.password).then(function (result) {
+          result
+            ? res.status(200).json({
+                message: "Login successful",
+                user,
+              })
+            : res.status(400).json({ message: "Login not succesful" })
+        })
+      }
+    } catch (error) {
+      res.status(400).json({
+        message: "An error occurred",
+        error: error.message,
       })
     }
   }
